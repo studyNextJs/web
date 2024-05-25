1dc48e4ad7cf94f7d2c5f456db907f963aa2591e
seulgi97 <lsg9709@naver.com>
2024-05-25 20:05:10 +0900

    add: 사이드바 제작완료

---
+++ app/sidebar.tsx
@@ -0,0 +1,14 @@
+const Sidebar = () => {
+    return (
+      <div className="sidebar">
+        <h2>Sidebar</h2>
+        <a href="#home">Home</a>
+        <a href="#services">Services</a>
+        <a href="#clients">Clients</a>
+        <a href="#contact">Contact</a>
+      </div>
+    );
+  };
+  
+  export default Sidebar;
+  
