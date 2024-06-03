b9e59b68ffc7055a30947fe07f55c5552027bd53
seulgi97 <lsg9709@naver.com>
2024-06-03 17:54:04 +0900

    add: loaclhost db 연결

--- surveyor/surveyor/settings.py
+++ surveyor/surveyor/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'surveyor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'surveyor',
+        'USER': 'admin',
+        'PASSWORD' : 'password',
+        'HOST' : 'localhost',
+        'PORT' :'',
     }
 }
 
