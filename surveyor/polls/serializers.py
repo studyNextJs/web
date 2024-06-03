4295fc972760f2317e53d2ec1566c0b0b38325a2
seulgi97 <lsg9709@naver.com>
2024-06-03 22:28:49 +0900

    add: 체크박스 기능설정을 위해 추가

--- surveyor/polls/serializers.py
+++ surveyor/polls/serializers.py
@@ -1,19 +1,25 @@
+# polls/serializers.py
+
 from rest_framework import serializers
 from .models import Survey, Question, Choice, Response, Answer
-
-class SurveySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Survey
-        fields = '__all__'
-
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = '__all__'
 
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
+        fields = '__all__'
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+class SurveySerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Survey
         fields = '__all__'
 
 class ResponseSerializer(serializers.ModelSerializer):
