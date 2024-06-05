c69423fd34ca1a28240cc7742c2f88721470e9d1
seulgi97 <lsg9709@naver.com>
2024-06-05 20:39:21 +0900

    add: 대답 완료시에 페이지 css 적용완료

--- survey-frontend/src/components/SurveyThanks.vue
+++ survey-frontend/src/components/SurveyThanks.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
-    <h1>Thank you for your response!</h1>
-    <router-link to="/">Back to Surveys</router-link>
-  </div>
+  <v-container>
+    <v-card class="thanks-card">
+      <v-card-title>
+        <h1>Thank you for your response!</h1>
+      </v-card-title>
+      <v-card-text>
+        <router-link to="/">
+          <v-btn color="primary" class="mx-2" @click="goToHome">Back to Surveys</v-btn>
+        </router-link>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToHome() {
+      this.$router.push('/');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.thanks-card {
+  text-align: center;
+  padding: 20px;
+  margin-top: 50px;
+}
+
+.v-btn {
+  margin-top: 20px;
+}
+</style>
