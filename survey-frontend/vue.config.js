5a5e365feed31a2a507eafce42812825c3e60d34
seulgi97 <lsg9709@naver.com>
2024-06-08 06:48:56 +0900

    test: css

--- survey-frontend/vue.config.js
+++ survey-frontend/vue.config.js
@@ -1,11 +1,13 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
+  publicPath: './', // 추가: 상대경로 설정
+  assetsDir: './', // 추가: 어셋 경로 설정
   devServer: {
     proxy: {
       '/polls': {
-        target: '/',  // 상대 경로로 수정
+        target: process.env.VUE_APP_API_BASE_URL,  
         changeOrigin: true,
       }
     }
