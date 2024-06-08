3cf57b56438950918dfb0508c37fd997ea49480c
seulgi97 <lsg9709@naver.com>
2024-06-09 00:15:59 +0900

    test: vue config

--- survey-frontend/vue.config.js
+++ survey-frontend/vue.config.js
@@ -2,18 +2,25 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
-  css: {
-    extract: process.env.NODE_ENV === 'production' ? {
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css',
-    } : false,
-  },
+  
+  // 배포 환경에 따라 publicPath 설정
+  publicPath: '/static',
+  
+  // 빌드 파일이 생성될 디렉토리
+  outputDir: 'dist',
+  
+  // 에셋 파일의 경로 설정
+  assetsDir: './',
+  
+  // 개발 서버 프록시 설정
   devServer: {
     proxy: {
       '/polls': {
-        target: 'https://seulgi97.duckdns.org',  
+        target: process.env.VUE_APP_API_BASE_URL,
         changeOrigin: true,
-      },
-    },
+      }
+    }
   },
+  
+  // 추가 설정을 여기에 추가할 수 있습니다...
 });
